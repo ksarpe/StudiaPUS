065a1e7457c9982480a8bc9f26359b5b52f9ac44
ksarpe <2299kasper@gmail.com>
2022-10-21 19:14:13 +0200

    client name + receiver name

--- src/pus2022/ClientThread.java
+++ src/pus2022/ClientThread.java
@@ -14,24 +14,42 @@ public class ClientThread implements Runnable {
     private final Socket socket;
     private BufferedReader input;
     private PrintWriter output;
+    public String name;
+    private String receiverName;
+
+
+    private String getName(String text){
+        output.println(text);
+        try {
+            return input.readLine();
+        } catch (IOException ex){
+            System.err.println(ex.getMessage());
+        }
+        return null;
+    }
     
     private int doConversation(BufferedReader input, PrintWriter output) {
         int numberOfLines = 0;
         output.println("Welcome, " + socket.getRemoteSocketAddress());
+        name = getName("Tell me your name: ");
         for(;;) {
+            receiverName = getName("Tell me receiver name: ");
             String line = null;
             try {
+                output.println("Write your message: ");
                 line = input.readLine();
                 numberOfLines++;
-            } catch(IOException ex) {}
+            } catch(IOException ex) {
+                System.err.println(ex.getMessage());
+            }
             if(line == null) break;
             if(line.startsWith("?")) {
                 output.println("Current number of threads: " + TcpServer.clientsPool.size());
             } else {
                 int counter = 0;
                 for(ClientThread clientThread: TcpServer.clientsPool) {
-                    if(clientThread != this) {
-                        clientThread.output.println(socket.getRemoteSocketAddress() + ">> " +line);
+                    if(clientThread != this && clientThread.name.equals(receiverName)) {
+                        clientThread.output.println(name + ">> " +line);
                         counter++;
                     }
                 }
--- src/pus2022/TcpServer.java
+++ src/pus2022/TcpServer.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 
 /**
  *
- * @author jaroc
+ * @author Kasper Janowski
  */
 public class TcpServer {
     
